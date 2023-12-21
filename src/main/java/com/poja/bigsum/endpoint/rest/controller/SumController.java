3179a2fb9ba324e62fbebf2d36acbdc1473e4808
Lova Fenohasina RAFANOMEZANTSOA <fenohasinalala@gmail.com>
2023-12-21 09:40:42 +0300

    feat: sum

--- src/main/java/com/poja/bigsum/endpoint/rest/controller/SumController.java
+++ src/main/java/com/poja/bigsum/endpoint/rest/controller/SumController.java
@@ -1,3 +1,20 @@
-public class SumController {
+package com.poja.bigsum.endpoint.rest.controller;
 
+import com.poja.bigsum.PojaGenerated;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigInteger;
+
+@PojaGenerated
+@RestController
+@AllArgsConstructor
+public class SumController {
+    @GetMapping("/big-sum")
+    public String bigSum(@RequestParam(value = "a") BigInteger a,@RequestParam(value = "b") BigInteger b) {
+        BigInteger sum = a.add(b);
+        return String.valueOf(sum);
+    }
 }
